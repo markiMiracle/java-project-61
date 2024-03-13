50d960212d4ac04577cf649a668b768d39b80eb1
markiMiracle <someonewhosome@list.ru>
2024-03-13 23:31:36 +0300

    adds

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -42,7 +42,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name?");
+        System.out.println("May I have your name?");
         name = scanner.next();
         System.out.println("Hello, " + name + "!");
     }
