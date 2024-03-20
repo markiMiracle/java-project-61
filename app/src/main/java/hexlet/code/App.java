aa174944023241744a52d35a44dce7902352a95d
markiMiracle <someonewhosome@list.ru>
2024-03-20 10:52:29 +0300

    fix something

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,17 +9,12 @@ import hexlet.code.games.Even;
 import java.util.Scanner;
 
 public class App {
-    public static String name = "name";
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n"
                     + "3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         Scanner sc = new Scanner(System.in);
         var numOfTheGame = sc.nextInt();
         System.out.println("Your choice: " + numOfTheGame + "\n");
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        name = sc.next();
-        System.out.println("Hello, " + name + "!");
         if (numOfTheGame == 0) {
             System.out.println("Leave the game...");
             System.exit(0);
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -6,27 +6,31 @@ public class Engine {
     public static final int ROUNDS = 3;
 
     public static void gameStart(String startMess, String[][] gameData) {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        var name = sc.next();
+        System.out.println("Hello, " + name + "!");
         System.out.println(startMess);
-        Scanner sq = new Scanner(System.in);
         boolean index = true;
         for (var i = 0; i < Engine.ROUNDS; i++) {
             var question = gameData[0][i];
             var answer = gameData[1][i];
             System.out.println("Question: " + question);
-            var input = sq.next();
+            var input = sc.next();
             if (input.equals(answer)) {
                 System.out.println("Correct!");
             } else {
                 System.out.println("'" + input + "' is wrong answer ;(. "
-                        + "Correct answer was " + "'" + answer + "'.\nLet's try again, " + App.name + "!");
+                        + "Correct answer was " + "'" + answer + "'.\nLet's try again, " + name + "!");
                 index = false;
                 break;
             }
         }
         if (index) {
-            System.out.println("Congratulations, " + App.name + "!");
+            System.out.println("Congratulations, " + name + "!");
         }
-        sq.close();
+        sc.close();
     }
 
     public static class Utils {
